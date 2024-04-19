ffbf6fcdee2f8cf076e2bcfdd6a3a1fb9570de11
vmllmv <v.lafaverges@gmail.com>
2024-04-19 11:00:51 +0200

    date creation ajouter a la vue de listing admin et une ligne/2 fond différent

--- blog/models/Article.php
+++ blog/models/Article.php
@@ -4,19 +4,19 @@
  * Entité Article, un article est défini par les champs
  * id, id_user, title, content, date_creation, date_update
  */
- class Article extends AbstractEntity 
- {
-    private int $idUser;
-    private string $title = "";
-    private string $content = "";
+class Article extends AbstractEntity
+{
+    private int       $idUser;
+    private string    $title        = "";
+    private string    $content      = "";
     private ?DateTime $dateCreation = null;
-    private ?DateTime $dateUpdate = null;  
+    private ?DateTime $dateUpdate   = null;
 
     /**
-     * Setter pour l'id de l'utilisateur. 
+     * Setter pour l'id de l'utilisateur.
      * @param int $idUser
      */
-    public function setIdUser(int $idUser) : void 
+    public function setIdUser(int $idUser): void
     {
         $this->idUser = $idUser;
     }
@@ -25,7 +25,7 @@
      * Getter pour l'id de l'utilisateur.
      * @return int
      */
-    public function getIdUser() : int 
+    public function getIdUser(): int
     {
         return $this->idUser;
     }
@@ -34,7 +34,7 @@
      * Setter pour le titre.
      * @param string $title
      */
-    public function setTitle(string $title) : void 
+    public function setTitle(string $title): void
     {
         $this->title = $title;
     }
@@ -43,7 +43,7 @@
      * Getter pour le titre.
      * @return string
      */
-    public function getTitle() : string 
+    public function getTitle(): string
     {
         return $this->title;
     }
@@ -52,12 +52,12 @@
      * Setter pour le contenu.
      * @param string $content
      */
-    public function setContent(string $content) : void 
+    public function setContent(string $content): void
     {
         $this->content = $content;
     }
 
-    
+
     /**
      * Getter pour le contenu.
      * Retourne les $length premiers caractères du contenu.
@@ -66,7 +66,7 @@
      * Si le contenu est plus grand que $length, on retourne les $length premiers caractères avec "..." à la fin.
      * @return string
      */
-    public function getContent(int $length = -1) : string 
+    public function getContent(int $length = -1): string
     {
         if ($length > 0) {
             // Ici, on utilise mb_substr et pas substr pour éviter de couper un caractère en deux (caractère multibyte comme les accents).
@@ -83,9 +83,9 @@
      * Setter pour la date de création. Si la date est une string, on la convertit en DateTime.
      * @param string|DateTime $dateCreation
      * @param string $format : le format pour la convertion de la date si elle est une string.
-     * Par défaut, c'est le format de date mysql qui est utilisé. 
+     * Par défaut, c'est le format de date mysql qui est utilisé.
      */
-    public function setDateCreation(string|DateTime $dateCreation, string $format = 'Y-m-d H:i:s') : void 
+    public function setDateCreation(string|DateTime $dateCreation, string $format = 'Y-m-d H:i:s'): void
     {
         if (is_string($dateCreation)) {
             $dateCreation = DateTime::createFromFormat($format, $dateCreation);
@@ -96,10 +96,20 @@
     /**
      * Getter pour la date de création.
      * Grâce au setter, on a la garantie de récupérer un objet DateTime.
-     * @return DateTime
+     * @return DateTime|IntlDateFormatter |string
      */
-    public function getDateCreation() : DateTime 
+    public function getDateCreation(): DateTime|IntlDateFormatter |string
     {
+
+        if (!is_string($this->dateCreation)) {
+            $dateCreation = new IntlDateFormatter('fr_FR',IntlDateFormatter::FULL,
+                IntlDateFormatter::FULL,
+                'Europe/Paris',
+                IntlDateFormatter::GREGORIAN,
+                'MM/dd/yyyy');
+           return $dateCreation->format($this->dateCreation);
+
+        }
         return $this->dateCreation;
     }
 
@@ -109,10 +119,8 @@
      * @param string $format : le format pour la convertion de la date si elle est une string.
      * Par défaut, c'est le format de date mysql qui est utilisé.
      */
-    public function setDateUpdate(string|DateTime $dateUpdate, string $format = 'Y-m-d H:i:s') : void 
+    public function setDateUpdate(string|DateTime $dateUpdate, string $format = 'Y-m-d H:i:s'): void
     {
-        var_dump($dateUpdate);
-
         if (is_string($dateUpdate)) {
             $dateUpdate = DateTime::createFromFormat($format, $dateUpdate);
         }
@@ -125,8 +133,8 @@
      * si la date de mise à jour n'a pas été définie.
      * @return DateTime|null
      */
-    public function getDateUpdate() : ?DateTime 
+    public function getDateUpdate(): ?DateTime
     {
         return $this->dateUpdate;
     }
- }
+}
--- blog/views/templates/admin.php
+++ blog/views/templates/admin.php
@@ -1,8 +1,8 @@
-<?php 
-    /** 
-     * Affichage de la partie admin : liste des articles avec un bouton "modifier" pour chacun. 
-     * Et un formulaire pour ajouter un article. 
-     */
+<?php
+/**
+ * Affichage de la partie admin : liste des articles avec un bouton "modifier" pour chacun.
+ * Et un formulaire pour ajouter un article.
+ */
 ?>
 
 <h2>Edition des articles</h2>
@@ -12,8 +12,16 @@
         <div class="articleLine">
             <div class="title"><?= $article->getTitle() ?></div>
             <div class="content"><?= $article->getContent(200) ?></div>
-            <div><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></div>
-            <div><a class="submit" href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?> >Supprimer</a></div>
+
+            <div><a class="submit"
+                    href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></div>
+            <div><a class="submit"
+                    href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?> >Supprimer</a>
+            </div>
+            <div class="content">nb vue</div>
+            <div class="content">nb comments</div>
+            <div class="content">date pubblication <?= $article->getDateCreation ()?></div>
+
         </div>
     <?php } ?>
 </div>
