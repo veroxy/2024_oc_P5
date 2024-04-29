05e2fa25bd94d297b5e3053e0fcdfcfaada6b506
vmllmv <v.lafaverges@gmail.com>
2024-04-29 23:58:16 +0200

    design administration OK en version tableau

--- blog/views/templates/admin.php
+++ blog/views/templates/admin.php
@@ -8,27 +8,35 @@
 <!--<h2>Edition des articles</h2>-->
 <h2>Admin Monitoring</h2>
 
-<div class="adminArticle">
+<table class="adminArticle">
+    <tr class="articleHeaderLine">
+        <th class="title">title</th>
+        <th class="content">short desc</th>
+        <th class="t-cell">nb views</th>
+        <th class="t-cell">nb commentaires</th>
+        <th class="t-cell">date pubblication</th>
+        <th class="t-cell" colspan="3">actions</th>
+        <!--        <th class="t-cell"></th>-->
+        <!--        <th class="t-cell"></th>-->
+    </tr>
     <?php foreach ($articles as $article) { ?>
-        <div class="articleLine">
-            <div class="title"><?= $article->getTitle() ?></div>
-            <div class="content"><?= $article->getContent(200) ?></div>
-            <div class="content" id="<?= 'page_views_' . $article->getSlug() ?>">
-                nb vues
-                : <?= $article->getViews() >= 1 ? $article->getViews() : (isset($_SESSION['page_views_' . $article->getSlug()]) ? $_SESSION['page_views_' . $article->getSlug()] : 0); ?></div>
-            <div class="content">nb commentaires : <?= count($article->getComments()) ?></div>
-            <div class="content">date pubblication
-                : <?= ucfirst(Utils::convertDateToFrenchFormat($article->getDateCreation())) ?></div>
-
-            <div><a class="submit"
-                    href="index.php?action=showArticle&id=<?= $article->getId() ?>">voir</a></div>
-            <div><a class="submit"
-                    href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></div>
-            <div><a class="submit"
-                    href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?> >Supprimer</a>
-            </div>
-        </div>
+<!--        <tr class="">-->
+        <tr class="articleLine">
+            <td class="title"><?= $article->getTitle() ?></td>
+            <td class="content"><?= $article->getContent(200) ?></td>
+            <td class="other"
+                id="<?= 'page_views_' . $article->getSlug() ?>"><?= $article->getViews() >= 1 ? $article->getViews() : (isset($_SESSION['page_views_' . $article->getSlug()]) ? $_SESSION['page_views_' . $article->getSlug()] : 0); ?></td>
+            <td class="other"><?= count($article->getComments()) ?></td>
+            <td class="other"><?= ucfirst(Utils::convertDateToFrenchFormat($article->getDateCreation())) ?></td>
+            <td class="action"><a class="submit"
+                   href="index.php?action=showArticle&id=<?= $article->getId() ?>">voir</a></td>
+            <td class="action"><a class="submit"
+                   href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></td>
+            <td class="action"><a class="submit"
+                   href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?> >Supprimer</a>
+            </td>
+        </tr>
     <?php } ?>
-</div>
+</table>
 
 <a class="submit" href="index.php?action=showUpdateArticleForm">Ajouter un article</a>
